c1fc1418b8725885b46943e7dbd9906bfb62fb95
FabianMenjura <53685320+fabianmenjura@users.noreply.github.com>
2024-05-27 16:30:10 -0500

    ✈️ Despliegue - Coleccion de publico objetivo, también se crean relaciones entre empresa, publico objetivo y motivadors

---
+++ src/api/publico-objetivo/controllers/publico-objetivo.js
@@ -0,0 +1,8 @@
+'use strict';
+/**
+ * publico-objetivo controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::publico-objetivo.publico-objetivo');
---
+++ src/api/publico-objetivo/services/publico-objetivo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * publico-objetivo service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::publico-objetivo.publico-objetivo');
